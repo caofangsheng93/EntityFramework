39ab5a1db984ea420d329728da6bcd6e546875fc
Rowan Miller <mail@romiller.com>
2016-05-26 16:02:35 -0700

    :memo:  Docs for remaining public APIs in core

--- src/Microsoft.EntityFrameworkCore/Infrastructure/IModelCustomizer.cs
+++ src/Microsoft.EntityFrameworkCore/Infrastructure/IModelCustomizer.cs
@@ -5,8 +5,26 @@ using JetBrains.Annotations;
 
 namespace Microsoft.EntityFrameworkCore.Infrastructure
 {
+    /// <summary>
+    ///     <para>
+    ///         Builds the model for a given context.
+    ///     </para>
+    ///     <para>
+    ///         This interface is typically used by database providers (and other extensions). It is generally
+    ///         not used in application code.
+    ///     </para>
+    /// </summary>
     public interface IModelCustomizer
     {
+        /// <summary>
+        ///     Builds the model for a given context.
+        /// </summary>
+        /// <param name="modelBuilder">
+        ///     The builder being used to construct the model.
+        /// </param>
+        /// <param name="dbContext">
+        ///     The context instance that the model is being created for.
+        /// </param>
         void Customize([NotNull] ModelBuilder modelBuilder, [NotNull] DbContext dbContext);
     }
 }
